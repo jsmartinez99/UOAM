24afeea07f4cf37edc5b38ac8334d4de7e88a1e3
Sebastian <sebasmartinez@gmail.com>
2026-07-27 00:54:07 -0300

    feat: add password migration script and update project dependencies

---
+++ update_passwords.sql
@@ -0,0 +1,3 @@
+UPDATE users SET "hashedPassword" = '$2b$12$EtCsYksysrHADrnQnJ8i8ugNFd1g787uhjfE/w6l.PQJFPyT1Y0aq' WHERE email = 'admin@uoam.com';
+UPDATE users SET "hashedPassword" = '$2b$12$I/0nMC/XQYuKtMM1SBhOIOCKdobvs9fiiA0CNuct8I/J7R0WIVuEO' WHERE email = 'arranger@uoam.com';
+UPDATE users SET "hashedPassword" = '$2b$12$v/Xp.vnIA/4e4AYalBTKS.zmY97oEj6hhfGdeNUg/WjvW891JMXv6' WHERE email = 'standard@uoam.com';
